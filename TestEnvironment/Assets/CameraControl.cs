3a79865e5c23e65feeecff52e06eb9ce9284eb38
csewell8 <csewell8164@gmail.com>
2020-09-25 22:40:23 -0400

    Object detection and Camera swivel
    --Began adding camera rotation for bird
      -Needs Bird model rotated -90 degrees/90 degrees
    
    --Began adding consumable detector
      -Allows Bird to see when there is a fruit/object in front of it
      -Would like to implement the bird looking at the closest object to it.

---
+++ TestEnvironment/Assets/CameraControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public Camera cam;
+    public GameObject player;
+    private float rotationAngle = 100f;
+    public Vector3 CameraOffset;
+    private float cameraRotationNormalizer = 90.0f;
+    private float birdAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationAngle = 100f;
+       // CameraOffset = new Vector3(player.transform.position.x - 0.38f, player.transform.position.y + 3.03f, player.transform.position.z + 7.8f);
+    }
+
+    private void LateUpdate()
+    {
+       // CameraOffset = new Vector3(player.transform.position.x - 0.38f, player.transform.position.y + 3.03f, player.transform.position.z + 7.8f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        birdAngle = player.transform.rotation.eulerAngles.y;
+
+        float normalizedCam = ((cam.transform.rotation.eulerAngles.y - cameraRotationNormalizer)) % 360;
+        if(normalizedCam > 180.0f)
+        {
+            normalizedCam = normalizedCam - 360f;
+        }
+
+        float modNum = (-10 ) % 360;
+        Debug.Log(modNum.ToString());
+        if (Input.GetKey(KeyCode.Q))
+       {
+           //Rotate camera around the bird clockwise
+           cam.transform.RotateAround(player.transform.position, new Vector3(0,1,0), (float)(rotationAngle * Time.deltaTime));
+            Debug.Log(normalizedCam.ToString());
+        }
+      else if(Input.GetKey(KeyCode.E))
+      {
+            //Rotate camera around the bird counterclockwise
+            cam.transform.RotateAround(player.transform.position, new Vector3(0, 1, 0), -(float)(rotationAngle * Time.deltaTime));
+            Debug.Log(normalizedCam.ToString());
+        }
+      else
+      {
+            if(normalizedCam > 0.5f || normalizedCam < -0.5f)
+            {
+                float diff = normalizedCam;
+                cam.transform.RotateAround(player.transform.position, new Vector3(0, 1, 0), -(float)(normalizedCam/5));
+            }
+            //Nothing pressed so just rotate back to behind the bird
+            else
+            {
+                cam.transform.RotateAround(player.transform.position, new Vector3(0, 1, 0), -(float)(normalizedCam));
+            }
+      }
+    
+    }
+}
--- TestEnvironment/Assets/FlightScripts/CockatielFlightScript.cs
+++ TestEnvironment/Assets/FlightScripts/CockatielFlightScript.cs
@@ -1,32 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class CockatielFlightScript : MonoBehaviour
 {
     public GameObject Cockatiel;
+    //public Collision birdfeet;
     public float lift = 2.0f;
     public float flapCoolDown = 0;
     public float flapCoolDownTime = 30;
     public Camera cam;
     public Rigidbody rb;
-    public float forwardMovement = 2.0f;
+    public float forwardMovement = 0.75f;
     public float rotationSpeed = 3.0f;
     public Vector3 forward;
+    private const int flying = 0b0001;
+    private const int walking = 0b0010;
+    
+    public int movementState = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        movementState = flying;
     }
 
+    void OnCollisionEnter(Collision collider)
+    {
+     //   Debug.Log(collider.gameObject.tag.ToString());
+        //Lots of different conditions will need to be established
+     //   if (collider.gameObject.CompareTag("Ground"))
+        {
+
+            movementState = walking;
+            //Something sus about what the actual values mean, RigidBody Info tab shows ~-20 for negative y when Debug says -10
+            if (rb.velocity.y > 10 || rb.velocity.y < -10)
+            {
+              //  Debug.Log("Velocity too high");
+             //   Debug.Log(rb.velocity.y.ToString());
+                rb.constraints = RigidbodyConstraints.None;//(rb.constraints ^ (RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationX));
+                
+            }
+            
+           
+        }
+    }
+    private void OnCollisionExit(Collision collider)
+    {
+//        if (collider.gameObject.CompareTag("Ground"))
+        {
+            movementState = flying;
+        }
+    }
+    protected void LateUpdate()
+    {
+        transform.localEulerAngles = new Vector3(0,  transform.localEulerAngles.y, 0);
+    }
     // Update is called once per frame
     void Update()
     {
         forward = transform.forward;
         flapCoolDown += Time.deltaTime;
+
         if(transform.rotation.x != 0)
         {
-            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+           transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
